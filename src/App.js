d3759302c830d7c4ab8cf695e88fe56f7bfd3b17
vitamix159 <97109007+vitamix159@users.noreply.github.com>
2022-05-11 11:54:01 +0300

    Added GitHub repository URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
-}
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+        <p>
+          This project was coded by Vitalija Ramonaitė and{" "}
+            <a 
+              href="https://github.com/vitamix159/react-weather-application"
+              target="_blank">
+              is open-sourced on GitHub
+            </a>
+        </p>
+      </div>
+    );
+  }
 
 
