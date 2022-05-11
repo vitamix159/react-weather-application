81d59b240f582578cb4c426da147b741be3e605f
vitamix159 <97109007+vitamix159@users.noreply.github.com>
2022-05-11 12:43:06 +0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
         <p>
           This project was coded by Vitalija Ramonaitė and{" "}
@@ -13,6 +14,7 @@ export default function App() {
               is open-sourced on GitHub
             </a>
         </p>
+        </div>
       </div>
     );
   }
