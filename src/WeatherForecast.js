2e5bb0e0ccb8e19111bc0f627a8501fe6fc36386
vitamix159 <97109007+vitamix159@users.noreply.github.com>
2022-06-21 00:13:22 +0300

    Fixed forecast bug

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
-import { useState } from "react";
 import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
     let [loaded, setLoaded] = useState(false);
     let [forecast, setForecast] = useState(null);
 
+    useEffect(() => {
+        setLoaded(false);
+    }, [props.coordinates]);
 
     function handleTheResponse(response) {
     setForecast(response.data.daily);
