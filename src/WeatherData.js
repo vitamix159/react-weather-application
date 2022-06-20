591e33b9909e080a9e1dacd3d72aae4607e5da2c
vitamix159 <97109007+vitamix159@users.noreply.github.com>
2022-06-21 00:01:32 +0300

    Added the remaining forecast days

--- src/WeatherData.js
+++ src/WeatherData.js
@@ -19,13 +19,13 @@ export default function WeatherData(props) {
         <div className="WeatherDescription mt-3">
           <div className="row">
             <div className="col-6">
-              <span className="d-flex">
+              <span className="d-flex mb-3">
               <WeatherIcon code={props.data.icon} size={80} />
               <WeatherTemperatureUnits celsius={props.data.temperature} />
               </span>
             </div>
             <div className="col-6">
-              <ul className="WeatherDesc">
+              <ul className="WeatherDesc mt-3">
                 <li>Humidity: {props.data.humidity}%</li>
                 <li>Wind: {Math.round(props.data.wind)}km/h</li>
                 <li>Feels like: {Math.round(props.data.feelsLike)}°C</li>
